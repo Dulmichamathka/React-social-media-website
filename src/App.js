c3423e40fdb4441a80155e3797bdccba26f57305
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-01-18 09:47:14 +0530

    positioning and adding styles for sidebar,feed and rightbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import Feed from "./Components/Feed";
+import Navbar from "./Components/Navbar";
+import Rightbar from "./Components/Rightbar";
+import Sidebar from "./Components/Sidebar";
+import { Box, Stack } from "@mui/material";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Box>
+      <Navbar />
+
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
