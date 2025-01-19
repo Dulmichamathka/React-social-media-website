a532b16fc48d4bff2fbaeafa397a3fa908d94e2b
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-01-19 16:43:49 +0530

    making crate post box

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -1,7 +1,106 @@
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  styled,
+  TextField,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import VideoChatIcon from "@mui/icons-material/VideoChat";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import Image from "@mui/icons-material/Image";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+const StyleModal = styled(Modal)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const UserBox = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "20px",
+});
 
 const Add = () => {
-  return <></>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Tooltip
+        onClick={(e) => setOpen(true)}
+        title="Add Post"
+        sx={{
+          position: "fixed",
+          bottom: 20,
+          left: { xs: "calc(50% - 25px)", md: 30 },
+        }}
+      >
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+
+      <StyleModal
+        open={open}
+        onClose={(e) => setOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box width={400} height={280} bgcolor="white" p={3} borderRadius={5}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            color="gray"
+            textAlign="center"
+            component="h2"
+          >
+            Create Post
+          </Typography>
+          <UserBox>
+            <Avatar src="/profile.jpg" sx={{ width: 30, height: 30 }}></Avatar>
+            <Typography fontWeight={500} variant="span">
+              Dulmi Chamathka
+            </Typography>
+          </UserBox>
+          <TextField
+            sx={{ width: "100%" }}
+            id="standard-multiline-static"
+            multiline
+            rows={3}
+            placeholder="what's on your mind?"
+            variant="standard"
+          />
+          <Stack direction="row" gap={1} mt={2} mb={3}>
+            <EmojiEmotionsIcon color="primary" />
+            <Image color="secondary" />
+            <VideoChatIcon color="success" />
+            <PersonAddIcon color="error" />
+          </Stack>
+
+          <ButtonGroup
+            fullWidth
+            variant="contained"
+            aria-label="Basic button group"
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: "100px" }}>
+              <CalendarMonthIcon />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyleModal>
+    </>
+  );
 };
 
 export default Add;
