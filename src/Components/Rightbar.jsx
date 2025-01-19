9d543d1dc9ce7ad35075404a9664a41cbe9d8455
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2025-01-19 13:32:50 +0530

    latest photo feature created

--- src/Components/Rightbar.jsx
+++ src/Components/Rightbar.jsx
@@ -1,14 +1,30 @@
-import { Avatar, AvatarGroup, Box, Typography } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  ImageList,
+  ImageListItem,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Box position="fixed">
+      <Box position="fixed" width={300}>
         <Typography variant="p" sx={{ fontWeight: 300, color: "gray" }}>
           Online Friends
         </Typography>
-        <AvatarGroup max={7} sx={{ marginTop: 1 }}>
+        <AvatarGroup
+          max={7}
+          sx={{
+            marginTop: 1,
+            marginBottom: 1,
+            "& .MuiAvatarGroup-avatar": {
+              marginLeft: "-4px", // Applies the spacing override
+            },
+          }}
+        >
           <Avatar alt="Remy Sharp" src="/girl5.jpeg" />
           <Avatar alt="Travis Howard" src="/girl3.jpeg" />
           <Avatar alt="Cindy Baker" src="/boy1.jpeg" />
@@ -19,6 +35,55 @@ const Rightbar = () => {
           <Avatar alt="Trevor Henderson" src="/boy3.jpeg" />
           <Avatar alt="Trevor Henderson" src="/girl6.jpeg" />
         </AvatarGroup>
+
+        <Typography variant="p" mt={2} sx={{ fontWeight: 300, color: "gray" }}>
+          Latest Photoes
+        </Typography>
+
+        <ImageList
+          cols={3}
+          rowHeight={100}
+          gap={5}
+          sx={{ marginTop: 1, marginBottom: 1 }}
+        >
+          <ImageListItem sx={{ borderRadius: 3 }}>
+            <img
+              src="/girl.jpg"
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
+                objectFit: "cover", // Correct style for covering the image proportionally
+              }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="/girll2.jpg"
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
+                objectFit: "cover", // Correct style for covering the image proportionally
+              }}
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="/bmwc1.jpg"
+              style={{
+                width: "100%",
+                height: "100%",
+                borderRadius: "8px",
+                objectFit: "cover", // Correct style for covering the image proportionally
+              }}
+            />
+          </ImageListItem>
+        </ImageList>
+
+        <Typography variant="p" mt={2} sx={{ fontWeight: 300, color: "gray" }}>
+          Latest Conversations
+        </Typography>
       </Box>
     </Box>
   );
